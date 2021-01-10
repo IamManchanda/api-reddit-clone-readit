11483c6d668198f9aa7010716708728822bc3485
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-01-11 00:41:31 +0530

    createPost route

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,5 +1,30 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
+import Post from "../entities/Post";
+import auth from "../middlewares/auth";
+
+const createPost = async (req: Request, res: Response) => {
+  const { title, body, sub } = req.body;
+  const { user } = res.locals;
+
+  if (title.trim() === "") {
+    return res.status(400).json({
+      title: "Title must not be empty",
+    });
+  }
+
+  try {
+    const post = new Post({ title, body, user, subName: sub });
+    await post.save();
+    return res.status(200).json(post);
+  } catch (error) {
+    console.log({ error });
+    return res.status(500).json({
+      error: "Something went wrong",
+    });
+  }
+};
 
 const postsRoutes = Router();
+postsRoutes.post("/", auth, createPost);
 
 export default postsRoutes;
