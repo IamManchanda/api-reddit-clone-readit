12b6e67ec336ad0f7868a9ff2c2febdad8d06494
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-01-11 17:15:31 +0530

    createPost route updates

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,10 +1,12 @@
 import { Request, Response, Router } from "express";
 import Post from "../entities/Post";
 import auth from "../middlewares/auth";
+import User from "../entities/User";
+import Sub from "../entities/Sub";
 
 const createPost = async (req: Request, res: Response) => {
   const { title, body, sub } = req.body;
-  const { user } = res.locals;
+  const user: User = res.locals.user;
 
   if (title.trim() === "") {
     return res.status(400).json({
@@ -13,7 +15,9 @@ const createPost = async (req: Request, res: Response) => {
   }
 
   try {
-    const post = new Post({ title, body, user, subName: sub });
+    const subRecord = await Sub.findOneOrFail({ name: sub });
+
+    const post = new Post({ title, body, user, sub: subRecord });
     await post.save();
     return res.status(200).json(post);
   } catch (error) {
