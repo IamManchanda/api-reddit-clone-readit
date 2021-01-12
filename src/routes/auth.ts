7c0f8b8b44f9099d432efec73622dedf3c88532c
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-01-12 16:42:12 +0530

    map validation errors

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -6,6 +6,13 @@ import cookie from "cookie";
 import User from "../entities/User";
 import auth from "../middlewares/auth";
 
+const mapErrors = (errors: Object[]) => {
+  return errors.reduce((prev: any, error: any) => {
+    prev[error.property] = Object.entries(error.constraints)[0][1];
+    return prev;
+  }, {});
+};
+
 const register = async (req: Request, res: Response) => {
   const { email, username, password } = req.body;
 
@@ -28,8 +35,9 @@ const register = async (req: Request, res: Response) => {
 
     const user = new User({ email, username, password });
     errors = await validate(user);
+
     if (errors.length > 0) {
-      return res.status(400).json({ errors });
+      return res.status(400).json(mapErrors(errors));
     }
 
     await user.save();
