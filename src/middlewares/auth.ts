22592ceaf10ae096616023d49a1a30a71bf1bfb1
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-01-16 00:31:39 +0530

    Create user middleware & update auth middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,22 +1,12 @@
 import { NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken";
 import User from "../entities/User";
 
-const auth = async (req: Request, res: Response, next: NextFunction) => {
+const auth = async (_req: Request, res: Response, next: NextFunction) => {
   try {
-    const { token } = req.cookies;
-    if (!token) {
-      throw new Error("Unauthenticated");
-    }
-
-    const { username }: any = jwt.verify(token, process.env.JWT_SECRET!);
-
-    const user = await User.findOne({ username });
+    const user: User | undefined = res.locals.user;
     if (!user) {
       throw new Error("Unauthenticated");
     }
-
-    res.locals.user = user;
     return next();
   } catch (error) {
     return res.status(401).json({
