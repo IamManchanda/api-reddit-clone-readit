a73863cba3e85a8805d1987283394484138befd3
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-01-22 00:28:25 +0530

    Expose voteScore in Comment Entity

--- src/entities/Comment.ts
+++ src/entities/Comment.ts
@@ -1,4 +1,4 @@
-import { Exclude } from "class-transformer";
+import { Exclude, Expose } from "class-transformer";
 import {
   BeforeInsert,
   Column,
@@ -42,6 +42,11 @@ export default class Comment extends Entity {
   @Exclude()
   @OneToMany(() => Vote, (vote) => vote.comment)
   votes: Vote[];
+
+  @Expose()
+  get voteScore(): number {
+    return this.votes?.reduce((prev, curr) => prev + (curr.value || 0), 0);
+  }
 
   protected userVote: number;
   setUserVote(user: User) {
