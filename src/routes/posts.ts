f20847a1d701b11e104a5bedc425fa19bb7f38a9
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-01-28 16:07:07 +0530

    Updates In Read Posts api backend route
    for pagination/infinite loading feature

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -30,13 +30,18 @@ const createPost = async (req: Request, res: Response) => {
   }
 };
 
-const readPosts = async (_req: Request, res: Response) => {
+const readPosts = async (req: Request, res: Response) => {
+  const currentPage: number = (req.query.page || 0) as number;
+  const postsPerPage: number = (req.query.count || 8) as number;
+
   try {
     const posts = await Post.find({
       order: {
         createdAt: "DESC",
       },
       relations: ["comments", "votes", "sub"],
+      skip: currentPage * postsPerPage,
+      take: postsPerPage,
     });
 
     if (res.locals.user) {
