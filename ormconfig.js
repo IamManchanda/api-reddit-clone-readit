3a0bbaca506d37aa64d74a66f4bd59af6a41c87e
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-01-31 20:08:54 +0530

    Update orm config

--- ormconfig.js
+++ ormconfig.js
@@ -1,22 +1,10 @@
-const {
-  NODE_ENV,
-  DB_DIALECT,
-  DB_HOST,
-  DB_PORT,
-  DB_USERNAME,
-  DB_PASSWORD,
-  DB_DATABASE,
-} = process.env;
+const { NODE_ENV } = process.env;
 
 const rootDir = NODE_ENV === "development" ? "src" : "build";
 
 module.exports = {
-  type: DB_DIALECT,
-  host: DB_HOST,
-  port: Number(DB_PORT),
-  username: DB_USERNAME,
-  password: DB_PASSWORD,
-  database: DB_DATABASE,
+  type: "postgres",
+  url: DATABASE_URL,
   synchronize: false,
   logging: NODE_ENV === "development",
   entities: [`${rootDir}/entities/**/*{.ts,.js}`],
